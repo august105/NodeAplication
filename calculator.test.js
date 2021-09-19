836b7a9c37a34b429ec5bdeb41ec40e722119f2c
august105 <bettin783@gmail.com>
2021-09-19 14:10:41 -0300

    Creating testes

---
+++ calculator.test.js
@@ -0,0 +1,17 @@
+//import { expect, test } from '@jest/globals';
+//import exp from 'constants';
+//import * as calculator from './calculator.js';
+
+
+const calculator = require('./calculator')
+test("Testando soma de 1+1", () => {
+    expect(calculator.soma(1,1)).toBe(2);
+});
+
+test("testar divisao de 42/6", () =>{
+    expect(calculator.divisao(42,6)).toBe(7);
+});
+
+test("testar soma de 5+5", () => {
+    expect(calculator.soma(5,5)).toBe(11);
+});
